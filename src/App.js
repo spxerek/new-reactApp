f3e84f051ecaea92f0735fadacb017c07c56a0d5
spxerek <sarveshpingulkar1@gmail.com>
2024-05-07 11:49:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import ParticlesComponent from './components/particles';
+
+function App() {
+  return (
+    <div className="App">
+      <ParticlesComponent id="particles"/>
+      <div className='newtext'>
+          <p>
+            Spxerek
+          </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
